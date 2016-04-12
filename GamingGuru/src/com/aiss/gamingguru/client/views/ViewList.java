7a92af4ed89539ee035988693bc3dbf30a959b65
Domingo Muñoz Daza <dmunnoz96@gmail.com>
2016-04-12 18:14:48 +0200

    Primera Versión de la interfaz

--- GamingGuru/src/com/aiss/gamingguru/client/views/ViewList.java
+++ GamingGuru/src/com/aiss/gamingguru/client/views/ViewList.java
@@ -1,56 +1,36 @@
 package com.aiss.gamingguru.client.views;
 
-import com.aiss.gamingguru.client.GreetingService;
-import com.aiss.gamingguru.client.GreetingServiceAsync;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.core.client.Scheduler.ScheduledCommand;
 import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.MenuBar;
+import com.google.gwt.user.client.ui.Image;
 
 public class ViewList extends Composite {
-
-	@SuppressWarnings("unused")
-	private final GreetingServiceAsync GreetingService = GWT
-			.create(GreetingService.class);
-
 	// private final HorizontalPanel mainPanel;
 	private final AbsolutePanel mainPanel;
 
 	public ViewList() {
 		mainPanel = new AbsolutePanel();
 		initWidget(mainPanel);
-
-		ScheduledCommand cmd = new ScheduledCommand() {
-
-			@Override
-			public void execute() {
-				// TODO Auto-generated method stub
-
-			}
-		};
 		// Create a menu bar
 		MenuBar menu = new MenuBar();
-		menu.setAutoOpen(true);
-		menu.setHeight("60px");
-//		menu.getElement().setAttribute("menu", "menu");
-		menu.setAnimationEnabled(true);
+		Image mando = new Image("files/mando.png");
+		Image icon = new Image("files/mando.png");
+		Image fondo = new Image("files/negro.png");
+		Image acercaDe = new Image("files/acerca.png");
 
-		// Make some sub-menus that we will cascade from the top menu.
-		menu.addItem("          ", true, cmd);
-		menu.addItem("Acerca de", cmd);
-
-		// Make a new menu bar, adding a few cascading menus to it.
-
-		// Add it to the root panel.
-		Label manu = new Label(menu.getStyleName());
-		Label munu = new Label(String.valueOf(menu.getOffsetHeight()));
-
+		
+		fondo.setStyleName("background");
+		menu.setStyleName("menu");
+		mando.setStyleName("centered");
+		icon.setStyleName("menuIcon");
+		acercaDe.addStyleName("acerca");	
+		
+		mainPanel.add(acercaDe);
+		mainPanel.add(fondo);
+		mainPanel.add(icon);
+		mainPanel.add(mando);
 		mainPanel.add(menu);
-		mainPanel.add(munu);
-
-		mainPanel.add(manu);
 		// Create a sub menu of recent documents
 
 	}
