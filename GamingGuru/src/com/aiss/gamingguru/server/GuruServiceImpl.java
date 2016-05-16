5391381ae8ac15502dace4b743eac63fb8c104c5
Domingo Muñoz Daza <dmunnoz96@gmail.com>
2016-05-16 02:17:37 +0200

    Integración visual de Login

--- GamingGuru/src/com/aiss/gamingguru/server/GuruServiceImpl.java
+++ GamingGuru/src/com/aiss/gamingguru/server/GuruServiceImpl.java
@@ -84,6 +84,7 @@ public class GuruServiceImpl extends RemoteServiceServlet implements
 					.userAgent(
 							"Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.6) Gecko/20070802 SeaMonkey/1.1.4")
 					.ignoreHttpErrors(true).get();
+			System.out.println(doc.data());
 			Element nota1 = doc.select("[itemprop=ratingValue]").first();
 			Element nota2 = doc
 					.select("[data-event-tracking=Tracking|games_overview|Kubrick|Metascore]")
@@ -272,7 +273,6 @@ public class GuruServiceImpl extends RemoteServiceServlet implements
 						burl = false;
 					}
 				}
-
 			};
 			saxParser.parse(requestUrl, handler);
 
