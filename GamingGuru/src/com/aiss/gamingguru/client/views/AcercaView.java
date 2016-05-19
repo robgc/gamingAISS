cea6378b0c2c92d87d9239ce10c8c0b2c22d4f8e
Roberto García <rodanber@gmail.com>
2016-05-19 09:31:09 +0200

    Revert "Merge remote-tracking branch 'origin/Jose' into roberto2"
    This reverts commit eaba81d0ba766971d9dd97abf6a2776724b7925c, reversing
    changes made to 02c7d1c0867bc07f7fd51b5f12537ba8e092becf.

--- GamingGuru/src/com/aiss/gamingguru/client/views/AcercaView.java
+++ GamingGuru/src/com/aiss/gamingguru/client/views/AcercaView.java
@@ -5,11 +5,6 @@ import java.util.List;
 import java.util.Map;
 
 import com.aiss.gamingguru.client.GamingGuru;
-<<<<<<< HEAD
-import com.aiss.gamingguru.client.GreetingService;
-import com.aiss.gamingguru.client.GreetingServiceAsync;
-=======
->>>>>>> origin/master
 import com.aiss.gamingguru.shared.Alumno;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -18,34 +13,19 @@ import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.Composite;
-<<<<<<< HEAD
-import com.google.gwt.user.client.ui.FlexTable;
-=======
 import com.google.gwt.user.client.ui.HTML;
->>>>>>> origin/master
 import com.google.gwt.user.client.ui.MenuBar;
 import com.google.gwt.user.client.ui.Image;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class AcercaView extends Composite {
-<<<<<<< HEAD
-	// private final HorizontalPanel mainPanel;
-	private final GreetingServiceAsync GreetingService = GWT.create(GreetingService.class);
-	
-=======
 
->>>>>>> origin/master
 	private final AbsolutePanel mainPanel;
-	private final FlexTable alumnosPanel;
-	private final FlexTable descripcionPanel;
 
 	public AcercaView(Map<String, String> params) {
 		mainPanel = new AbsolutePanel();
 		initWidget(mainPanel);
-		alumnosPanel = new FlexTable();
-		descripcionPanel = new FlexTable();
-		
+
 		MenuBar menu = new MenuBar();
 		Image icon = new Image("files/mando.png");
 		Image fondo = new Image("files/negro.png");
@@ -57,47 +37,7 @@ public class AcercaView extends Composite {
 		mainPanel.add(fondo);
 		mainPanel.add(icon);
 		mainPanel.add(menu);
-		
-		
-		alumnosPanel.setStylePrimaryName("alumnoTable");
-		alumnosPanel.getRowFormatter().setStylePrimaryName(0, "firstRow");
-		alumnosPanel.setWidget(0, 0, new Label("Nombre"));
-		alumnosPanel.setWidget(0, 1, new Label("Email"));
 
-		GreetingService.getAlumnos(new AsyncCallback<List<Alumno>>() {
-
-<<<<<<< HEAD
-			public void onSuccess(List<Alumno> result) {
-				showAlumnos(result);
-			}
-
-			public void onFailure(Throwable caught) {
-				Window.alert("Error loading contacts: " + caught.getMessage());
-
-			}
-		});
-		
-		mainPanel.add(alumnosPanel);
-		
-		descripcionPanel.setStylePrimaryName("descripcionTable");
-		descripcionPanel.setWidget(1, 0, new Label("\n"));
-		
-		Label titulo = new Label("\nGAMINGURU");
-		titulo.setStylePrimaryName("titulo");
-
-		descripcionPanel.setWidget(2, 0, titulo);
-		descripcionPanel.setWidget(3, 0,
-				new Label("\nPresentamos GamingGuru. Una aplicación capaz de analizar tu "
-						+ "\nbiblioteca de Steam y ofrecerte recomendaciones personalizadas"
-						+ "\nsobre los juegos de tu género favorito en función de los valores "
-						+ "\nanalíticos de los juegos de dicha biblioteca. También te ofreceremos "
-						+ "\nenlaces directos a la compra de tus recomendaciones en "
-						+ "\nplataformas como Amazon o la propia tienda de Steam dejando a "
-						+ "\ntu elección para qué plataforma deseas hacer la compra."));
-
-		mainPanel.add(descripcionPanel);
-		
-=======
 		showAlumnos();
 
 		String desc = "<fieldset>";
@@ -114,7 +54,6 @@ public class AcercaView extends Composite {
 		description.setStyleName("description");
 		RootPanel.get("description").add(description);
 		// RootPanel.get("table").add(alumnosPanel);
->>>>>>> origin/master
 
 		icon.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
@@ -125,20 +64,6 @@ public class AcercaView extends Composite {
 			}
 		});
 	}
-<<<<<<< HEAD
-	
-	public final void showAlumnos(List<Alumno> AlumnosSimple) {
-
-		int i = 0;
-		for (Alumno key : AlumnosSimple) {
-			alumnosPanel.setWidget(i + 1, 0, new Label(key.getName()));
-			alumnosPanel.setWidget(i + 1, 1, new Label(key.getEmail()));
-			i++;
-		}
-
-	}
-}
-=======
 
 	public final void showAlumnos() {
 
@@ -159,4 +84,3 @@ public class AcercaView extends Composite {
 	}
 
 }
->>>>>>> origin/master
